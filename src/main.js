1cae5c9812ebf2a0ac39187a84e70a18b1a6cfd2
William Oldham <wegg7250@gmail.com>
2021-09-12 10:26:19 +0100

    Install font-awesome

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faExclamationTriangle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
+library.add(faExclamationTriangle)
+
 import './tailwind.css'
 
 Vue.config.productionTip = false
+
+Vue.component("FaIcon", FontAwesomeIcon)
 
 new Vue({
     render: h => h(App),
